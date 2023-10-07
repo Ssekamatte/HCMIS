5a87521a5444dd599141849c982c8fc6b590ce58
James Ssekamatte <ssekamj@gmail.com>
2023-10-07 14:12:34 +0300

    Updates7Oct2023

---
+++ Pages/BalanceScoreCard/PaymentSearchPanelModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReApp.Data
+{
+    public class PaymentSearchPanelModel
+    {
+        // [Required]
+        public string Id { get; set; } = null!;
+        public DateTime? startDate { get; set; }
+        public DateTime? endDate { get; set; }
+    }
+}
