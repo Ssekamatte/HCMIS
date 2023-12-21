3a653690cee6a409e7ee3caf61345c03b4df6c20
James Ssekamatte <ssekamj@gmail.com>
2023-12-21 17:42:53 +0300

    Updates 21Dec2023

---
+++ wwwroot/js/js/detailexpand.js
@@ -0,0 +1,5 @@
+﻿highlightHeader = function (colIndex, rowindex) {
+    var a = document.getElementsByClassName("e-row")[rowindex];
+    var b = a.getElementsByClassName('e-rowcell')[colIndex].style.backgroundColor = "pink";
+  
+};
